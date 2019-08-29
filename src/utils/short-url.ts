af7d7f734b85d4dd6bb45ac6fb55ac2b21767773
Darryl Fabian <darryl@leisue.com>
2019-08-29 11:15:25 +0800

    add short url by google firebase

---
+++ src/utils/short-url.ts
@@ -0,0 +1,22 @@
+import http from './http'
+export default class ShortURL {
+  private DOMAIN : string
+  constructor (domain: string) {
+    this.DOMAIN = domain
+  }
+  public generate (url: string) {
+    console.log('this.domain.concat(url): ', this.DOMAIN.concat(url))
+    return http({
+      url: 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=',
+      method: 'POST',
+      // "Content-Type": 'application/json',
+      data: {
+        longDynamicLink: `https://dqv3s.app.goo.gl?link=${this.DOMAIN.concat(url)}`
+      }
+    })
+    .then((response: any) => {
+      const {shortLink: url, error} = <any> response.data
+      return (url)
+    })
+  }
+}
