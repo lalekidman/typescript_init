c3c326b06d71208ad13392b5dd0ee2d60d11e9af
cedrick_vs_the_world <johncedrick.domingo@leisue.com>
2019-11-25 09:45:56 +0800

    notification for customer rate changes

--- src/routes/notifications.ts
+++ src/routes/notifications.ts
@@ -6,6 +6,7 @@ import * as RC from '../utils/response-codes'
 import { IRequest } from '../utils/interfaces';
 import * as appConstants from '../utils/constants'
 import * as regExp from '../utils/regularExpressions'
+import notification from  '../class/notification'
 import Queue from '../class/queue'
 export default class Route {
   /**
@@ -27,19 +28,25 @@ export default class Route {
     .then(async (branches) => {
       for (let i in branches) {
         // notify the branch
-        let customerRates = await Queue.getBranchCustomerRates(branches[i]._id, -1, 0)
+        const {_id} = branches[i]
+        let customerRates = await Queue.getBranchCustomerRates(_id, -1, 0)
         const {r1, r2} = customerRates
-        console.log(customerRates)
         if (!r1 && !r2) {
-          console.log(`CUSTOMER RATE NOTIFICATION: No comparison for branch ${branches[i]._id}`)
+          console.log(`CUSTOMER RATE NOTIFICATION: No comparison for branch ${_id}`)
         }
         if (r1 > r2) {
           // decrease
           console.log('DECREASE')
+          console.log(`r1: ${r1}, r2: ${r2},`)
+          // promt notification here
+          await notification.invokeNotif(_id, customerRates, appConstants.BRANCH_NOTIFICATION_TYPES.CUSTOMER_RATE_DECREASE)
         }
         if (r1 < r2) {
           // increase
           console.log('INCREASE')
+          console.log(`r1: ${r1}, r2: ${r2},`)
+          // promt notification here
+          await notification.invokeNotif(_id, customerRates, appConstants.BRANCH_NOTIFICATION_TYPES.CUSTOMER_RATE_INCREASE)
         }
       }
       response.sendStatus(HttpStatus.OK)
