a86e9bd844b5aaa3d09482afa5835f02e7051edc
Darryl Fabian <darryl@leisue.com>
2019-12-18 10:24:43 +0800

    update response codes

--- src/utils/response-codes.ts
+++ src/utils/response-codes.ts
@@ -1,20 +1,20 @@
 export const SYSTEM_ERROR = {
-  statusCode: 10401,
+  statusCode: 10400,
   error: 'SYSTEM_ERROR',
   source: ''
 }
 export const SERVER_FAILED = {
-  statusCode: 10402,
+  statusCode: 10500,
   error: 'SERVER_FAILED',
   source: ''
 }
 export const URL_NOT_FOUND = {
-  statusCode: 10403,
+  statusCode: 10404,
   error: 'URL_NOT_FOUND',
   source: ''
 }
 export const UNAUTHORIZED_REQUEST = {
-  statusCode: 10404,
+  statusCode: 10401,
   error: 'UNAUTHORIZED_REQUEST',
   source: ''
 }
@@ -31,22 +31,5 @@ export const INVALID_VARIABLE_TYPE = {
 export const INALID_VALUE = {
   statusCode: 10302,
   error: 'INALID_VALUE',
-  source: ''
-}
-
-
-export const INVALID_ACCESS_TOKEN_FORMAT = {
-  statusCode: 290,
-  error: 'INVALID_ACCESS_TOKEN_FORMAT',
-  source: 'Access token is required or please check if the token format is correct.'
-}
-export const ACCESS_TOKEN_EXPIRED = {
-  statusCode: 291,
-  error: 'ACCESS_TOKEN_EXPIRED',
-  source: 'token is expired.'
-}
-export const INVALID_EMPLOYEE_LEVEL = {
-  statusCode: 292,
-  error: 'INVALID_EMPLOYEE_LEVEL',
   source: ''
 }
