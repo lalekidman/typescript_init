64a335e656adf5911145a57ab3eeeac71c0e897b
Lale Fabian <lalekidman@gmail.com>
2020-04-03 14:42:07 +0800

    add index route

---
+++ src/routes/index.ts
@@ -0,0 +1,19 @@
+import * as express from 'express'
+import {Request, Response, NextFunction, Router} from 'express'
+import * as HttpStatus from 'http-status-codes'
+export class _Router {
+  /**
+   * @class initiate router class
+   */
+  private readonly router: Router
+  constructor () {
+    this.router = Router({mergeParams: true})
+  }
+
+  public expose () {
+    this.router.get('/', (req: Request, res: Response, next: NextFunction) => {
+      res.status(HttpStatus.OK).send({result: true})
+    })
+    return this.router
+  }
+}
