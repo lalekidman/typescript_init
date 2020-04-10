7887a57f1011826e030baa309a3f0b5874be61c9
Lale Fabian <lalekidman@gmail.com>
2020-04-10 20:15:57 +0800

    enhance image vlidation

--- src/utils/regex-validator.ts
+++ src/utils/regex-validator.ts
@@ -2,6 +2,6 @@ export const ValidateURL = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/
 
 export const ValidateMobileNo = /^(\+639|09|9)\d{9}$/g
 
-export const ValidateImage = /\.(jpg|png|gif|bmp)$/g
+export const ValidateImage = /\.(gif|png|jpeg|jpg|bmp)$/ig
 
 export const validateEmail = /^[a-zA-Z0-9\.\-]\@[a-zA-Z0-9]\.[a-zA-Z0-9]{1,10}$/g
