9734144ec8d08b5f2953586a3bc2a39d8fb3f83d
Lale Fabian <lalekidman@gmail.com>
2020-04-10 21:13:16 +0800

    update pattern

--- src/utils/regex-validator.ts
+++ src/utils/regex-validator.ts
@@ -4,6 +4,6 @@ export const MobileNumberPattern = /^(\+639|09|9)\d{9}$/g
 
 export const ImagePattern = /\.(gif|png|jpeg|jpg|bmp)$/ig
 
-export const EmailPattern = /^[a-zA-Z0-9\.\-]\@[a-zA-Z0-9]\.[a-zA-Z0-9]{1,10}$/g
+export const EmailPattern = /^[a-zA-Z0-9\./\-/]+\@[a-zA-Z0-9]{2,10}\.[a-zA-Z0-9]{1,7}$/g
 
 export const HumanNamePattern = /^[A-Za-z\-/\./]+$/g
