3f768e872a7ad15dfe83c0cf9557e7e46e1a494d
Lale Fabian <lalekidman@gmail.com>
2020-04-10 20:17:35 +0800

    just rename the regex validation pattern

--- src/utils/regex-validator.ts
+++ src/utils/regex-validator.ts
@@ -1,7 +1,9 @@
-export const ValidateURL = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/g
+export const URLPattern = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/g
 
-export const ValidateMobileNo = /^(\+639|09|9)\d{9}$/g
+export const MobileNumberPattern = /^(\+639|09|9)\d{9}$/g
 
 export const ImagePattern = /\.(gif|png|jpeg|jpg|bmp)$/ig
 
 export const validateEmail = /^[a-zA-Z0-9\.\-]\@[a-zA-Z0-9]\.[a-zA-Z0-9]{1,10}$/g
+
+export const HumanNamePattern = /^[A-Za-z\-/\./]+$/g
